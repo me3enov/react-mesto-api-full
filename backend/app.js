56eb9e5c1a8180d6ca3357993e217c98923f65f8
Artem Mezenov <art.mezenow@yandex.ru>
2021-06-24 18:03:42 +0300

    project_15 iteration_2

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const helmet = require('helmet');
+const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
@@ -18,6 +19,8 @@ const { PORT = 3000 } = process.env;
 const app = express();
 
 app.use(helmet());
+
+app.use(cookieParser());
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
