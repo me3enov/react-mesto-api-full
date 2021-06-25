a61d905ea012f6bdcca71eee39a96dc91aaa0086
Artem Mezenov <art.mezenow@yandex.ru>
2021-06-25 14:59:54 +0300

    project_15 iteration_3

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -4,13 +4,7 @@ const UnauthorizedError = require('../errors/UnauthorizedError');
 const { NODE_ENV, JWT_SECRET } = process.env;
 
 module.exports = (req, res, next) => {
-  const { authorization } = req.headers;
-
-  if (!authorization || !authorization.startsWith('Bearer ')) {
-    throw new UnauthorizedError('Authorization required');
-  }
-
-  const token = authorization.replace('Bearer ', '');
+  const token = req.cookies.jwt;
   let payload;
 
   try {
