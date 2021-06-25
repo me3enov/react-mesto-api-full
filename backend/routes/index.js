8632f5f8ad055d3471bb5a1644630abfdc06bf6a
Artem Mezenov <art.mezenow@yandex.ru>
2021-06-25 15:43:59 +0300

    project_15 iteration_3

--- backend/routes/index.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const userRoutes = require('./users');
-const cardRoutes = require('./cards');
-
-router.use(userRoutes);
-router.use(cardRoutes);
-
-module.exports = router;
