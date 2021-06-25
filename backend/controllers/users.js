2613e15440448cec37ea8bafb7f1fa642244945c
Artem Mezenov <art.mezenow@yandex.ru>
2021-06-25 16:38:01 +0300

    project_15 iteration_3

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -136,7 +136,6 @@ module.exports.login = (req, res, next) => {
       res
         .cookie('jwt', token, {
           maxAge: 3600000 * 24 * 7,
-          httpOnly: true,
           sameSite: 'None',
           secure: true,
         })
